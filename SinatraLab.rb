14bbd6adb153f2d52264588ff4cf5f1508410089
Noora <ndosari@andrew.cmu.edu>
2019-01-31 08:24:51 +0000

    Adding modified SinatraLab and response files

--- SinatraLab.rb
+++ SinatraLab.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
     erb :index
 end
+
+post '/' do
+    @fname = params[:firstname]
+    @lname = params[:lastname]
+    @class_love = params[:class_love]
+    @feedback = params[:feedback]
+    
+    @happy = (@class_love == "More than I care to say") ? 
+    "That's Great!" : "Well, hopefully you'll REALLY love it by the end"
+    
+    erb :response
+end
